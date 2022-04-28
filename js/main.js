c7dcafe292d722a6de3467b240c4cf562573bb53
hyeminem-Jo <konnimey@naver.com>
2022-04-28 16:24:51 +0900

    ✨ Implement stock tab and lnb tab

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// 쇼룸 탭 활성화
+// showroom 탭 활성화
 $('.showroom-btn .btn').click(function () {
   $(this).addClass('is-active')
   $(this).siblings('li').removeClass('is-active')
@@ -21,3 +21,21 @@ $('.quick-menu-btn').click(function() {
 // 서브페이지에 header, footer 연결
 $('.header-include').load('header.html')
 $('.footer-include').load('footer.html')
+
+// lnb 탭 활성화
+$('.lnb-list').click(function() {
+  $(this).addClass('is-active')
+  $(this).siblings().removeClass('is-active')
+  $(this).children('ul').stop().slideDown()
+  $(this).siblings().children('ul').stop().slideUp()
+})
+
+// stock 탭 활성화
+$('.stock .tab li').click(function() {
+  $(this).addClass('is-active')
+  $(this).siblings().removeClass('is-active')
+
+  var tab = $(this).attr('data-alt')
+  $('.stock #' + tab).addClass('is-active')
+  $('.stock #' + tab).siblings('li').removeClass('is-active')
+})
